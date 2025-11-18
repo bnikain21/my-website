f8dc58ccdda0836ae0d993bb1313e1a6215e4eb0
bnikain21 <bnikain@gmail.com>
2025-11-18 18:13:37 -0500

    fixed tab logo and icon

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Image from "next/image"
 
 function AboutMePage() {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center text-center w-full">
+        <h1 className="text-3xl text-text-e1m mb-3">About Me:</h1>
+        <Image src='/headshot.png' alt='headshot' width={300} height={700} />
+        
+        <p className="text-xl text-text-e1m pt-3 max-w-lg self-center">
+            I am a computer science graduate student at the Georgia Institute of Technology specializing 
+            in machine learning, with experience in software engineering, building AI-driven applications,
+            and working with computer networks.
+        </p>
     </div>
   )
 }
