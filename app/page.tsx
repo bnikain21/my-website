ccfe67d6563107540eb79ac8179a3915398533e4
bnikain21 <bnikain@gmail.com>
2025-11-18 21:20:30 -0500

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,14 @@ export default function Home() {
         <ContentCard title="Education" short_desc="Background" path="/education"/>  
         <ContentCard title="Projects" short_desc="Technical Work" path="/projects"/>    
       </div>
+      <div className="flex justify-center items-center h-20 mt-12">
+        <p className="text-center text-text-e1m text-sm">
+          Designed by me. 
+          Built using <a className="font-bold" href="https://nextjs.org/"> Next.js </a> and 
+          <a className="font-bold" href="https://tailwindcss.com/"> TailwindCSS</a>. Deployed using  
+           <a className="font-bold" href="https://aws.amazon.com/amplify/"> AWS Amplify. </a>
+        </p>
+      </div>
     </section>
     
   ); 
